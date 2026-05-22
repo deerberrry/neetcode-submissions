6ecf73931bb9fcc34ab85ffbfe39fc6603fbb556
deeksha <52229508+deerberrry@users.noreply.github.com>
2026-05-22 12:16:30 +0530

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.ts
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs: string[]): string {
+        let res= strs.map(str=>
+            str.length + "#"+ str
+        ).join("")
+        console.log('res', res);
+        return res;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str: string): string[] {
+        let result = []
+        let i =0, j=1;
+        while (i<str.length && j<str.length) { 
+            if(str[j] == "#") {
+                let length = Number(str.slice(i,j))
+                i= j+length+1;
+                result.push(str.slice(j+1, i))
+                j=i+1
+            }
+            else j++;
+        }
+        return result;
+    }
+}
