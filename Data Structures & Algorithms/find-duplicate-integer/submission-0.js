a1801487c916110c4de864aa342600ad3b436f9e
deeksha <52229508+deerberrry@users.noreply.github.com>
2026-06-01 16:46:52 +0530

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findDuplicate(nums) {
+        let seen = new Set();
+        for(let num of nums) {
+            if(seen.has(num)) return num;
+            else seen.add(num)
+        }
+        return -1;
+    }
+}
