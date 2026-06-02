619c30886429fdfa3427a3ee26886b33cc5208b5
deeksha <52229508+deerberrry@users.noreply.github.com>
2026-06-02 14:50:40 +0530

    Add: same-binary-tree - submission-0

---
+++ Data Structures & Algorithms/same-binary-tree/submission-0.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} p
+     * @param {TreeNode} q
+     * @return {boolean}
+     */
+    isSameTree(p, q) {
+        return this.dfs(p, q)
+    }
+
+    dfs(p, q) {
+        if(p== null && q==null) return true;
+        if(p==null || q==null || p.val != q.val) return false;
+        let left = this.dfs(p.left, q.left);
+        if(!left) return false;
+        let right = this.dfs(p.right, q.right);
+        if(!right) return false;
+        return true;
+    }
+}
