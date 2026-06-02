76c878cace36e4845bfa665561384fb30602abdc
deeksha <52229508+deerberrry@users.noreply.github.com>
2026-06-02 15:35:45 +0530

    Add: lowest-common-ancestor-in-binary-search-tree - submission-5

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-5.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {TreeNode} p
+     * @param {TreeNode} q
+     * @return {TreeNode}
+     */
+    lowestCommonAncestor(root, p, q) {
+        // if(!root || !p || !q) return root;
+        // if(p.val === root.val || q.val === root.val ||
+        //      (root.val > p.val && root.val < q.val) ||
+        //      (root.val < p.val && root.val > q.val)
+        // )
+        //     return root;
+        // if(p.val < root.val) 
+        //     return this.lowestCommonAncestor(root.left, p, q)
+        // else return this.lowestCommonAncestor(root.right, p, q)
+        if(!root) return null;
+        if(p.val < root.val && q.val < root.val) 
+            return this.lowestCommonAncestor(root.left, p, q)
+        else if(p.val > root.val && q.val > root.val) 
+            return this.lowestCommonAncestor(root.right, p, q)
+        else return root;
+    }
+}
